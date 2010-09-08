37f19d539b9699fad7eae8a49a3bcd8123b2a9c3
Chris Tacke <ctacke@users.noreply.github.com>
2010-09-08 21:30:57 +0000

    Added project skeleton (no implementation) for OpenNETCF.ORM.SQLite

---
+++ OpenNETCF.ORM.SQLite/SQLiteDataStore.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace OpenNETCF.ORM.SQLite
+{
+    public class SQLiteDataStore : DataStore<SQLiteEntityInfo>, IDisposable
+    {
+        public override void CreateStore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeleteStore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool StoreExists
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override void Insert(object item, bool insertReferences)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T[] Select<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T Select<T>(object primaryKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T[] Select<T>(string searchFieldName, object matchValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(object item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete(object item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete<T>(object primaryKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void FillReferences(object instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T[] Fetch<T>(int fetchCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T[] Fetch<T>(int fetchCount, int firstRowOffset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T[] Fetch<T>(string searchFieldName, int fetchCount, int firstRowOffset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Count<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete<T>(string fieldName, object matchValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
