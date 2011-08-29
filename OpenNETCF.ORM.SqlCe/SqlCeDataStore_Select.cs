943e01c25e24d07cff6171260d0880cc50694600
ctacke_cp <SND\ctacke_cp>
2011-08-29 14:59:25 +0000

    Added command caching to SqlCeStore.

--- OpenNETCF.ORM.SqlCe/SqlCeDataStore_Select.cs
+++ OpenNETCF.ORM.SqlCe/SqlCeDataStore_Select.cs
@@ -9,6 +9,15 @@ namespace OpenNETCF.ORM
 {
     partial class SqlCeDataStore
     {
+        private const int CommandCacheMaxLength = 10;
+        private Dictionary<string, SqlCeCommand> m_commandCache = new Dictionary<string, SqlCeCommand>();
+
+        /// <summary>
+        /// Determines if the ORM engine should be allowed to cache commands of not.  If you frequently use the same FilterConditions on a Select call to a single entity, 
+        /// using the command cache can improve performance by preventing the underlying SQL Compact Engine from recomputing statistics.
+        /// </summary>
+        public bool UseCommandCache { get; set; }
+
         /// <summary>
         /// Retrieves a single entity instance from the DataStore identified by the specified primary key value
         /// </summary>
@@ -220,7 +229,7 @@ namespace OpenNETCF.ORM
                 sb.Append(i == 0 ? " WHERE " : " AND ");
 
                 var filter = filters.ElementAt(i);
-                sb.Append(filter.FieldName);
+                sb.Append("[" + filter.FieldName + "]");
 
                 switch (filters.ElementAt(i).Operator)
                 {
@@ -251,8 +260,28 @@ namespace OpenNETCF.ORM
                 var param = new SqlCeParameter(paramName, filter.Value ?? DBNull.Value);
                 command.Parameters.Add(param);
             }
+            var sql = sb.ToString();
+            command.CommandText = sql;
 
-            command.CommandText = sb.ToString();
+            if (UseCommandCache)
+            {
+                if (m_commandCache.ContainsKey(sql))
+                {
+                    command.Dispose();
+                    command = m_commandCache[sb.ToString()];
+                }
+                else
+                {
+                    m_commandCache.Add(sql, command);
+
+                    // trim the cache so it doesn't grow infinitely
+                    if (m_commandCache.Count > CommandCacheMaxLength)
+                    {
+                        m_commandCache.Remove(m_commandCache.First().Key);
+                    }
+                }
+            }
+
             return command;
         }
 
@@ -271,187 +300,187 @@ namespace OpenNETCF.ORM
             bool tableDirect;
 
             var connection = GetConnection(false);
+            SqlCeCommand command = null;
+
             try
             {
                 CheckOrdinals(entityName);
 
-                using (var command = GetSelectCommand(entityName, filters, out tableDirect))
+                command = GetSelectCommand(entityName, filters, out tableDirect);
+                command.Connection = connection as SqlCeConnection;
+
+                int searchOrdinal = -1;
+                ResultSetOptions options = ResultSetOptions.Scrollable;
+
+                object matchValue = null;
+                string matchField = null;
+
+                if (tableDirect) // use index
                 {
-                    command.Connection = connection as SqlCeConnection;
-
-                    int searchOrdinal = -1;
-                    ResultSetOptions options = ResultSetOptions.Scrollable;
-
-                    object matchValue = null;
-                    string matchField = null;
-
-                    if (tableDirect) // use index
+                    if ((filters != null) && (filters.Count() > 0))
                     {
-                        if ((filters != null) && (filters.Count() > 0))
+                        var filter = filters.First();
+
+                        matchValue = filter.Value ?? DBNull.Value;
+                        matchField = filter.FieldName;
+
+                        var sqlfilter = filter as SqlFilterCondition;
+                        if ((sqlfilter != null) && (sqlfilter.PrimaryKey))
                         {
-                            var filter = filters.First();
-
-                            matchValue = filter.Value ?? DBNull.Value;
-                            matchField = filter.FieldName;
-
-                            var sqlfilter = filter as SqlFilterCondition;
-                            if ((sqlfilter != null) && (sqlfilter.PrimaryKey))
-                            {
-                                searchOrdinal = Entities[entityName].PrimaryKeyOrdinal;
-                            }
-                        }
-
-                        // we need to ensure that the search value does not exceed the length of the indexed
-                        // field, else we'll get an exception on the Seek call below
-                        var indexInfo = GetIndexInfo(command.IndexName);
-                        if (indexInfo != null)
-                        {
-                            if(indexInfo.MaxCharLength > 0)
-                            {
-                                var value = (string)matchValue;
-                                if (value.Length > indexInfo.MaxCharLength)
-                                {
-                                    matchValue = value.Substring(0, indexInfo.MaxCharLength);
-                                }
-                            }
+                            searchOrdinal = Entities[entityName].PrimaryKeyOrdinal;
                         }
                     }
 
-                    using (var results = command.ExecuteResultSet(options))
+                    // we need to ensure that the search value does not exceed the length of the indexed
+                    // field, else we'll get an exception on the Seek call below
+                    var indexInfo = GetIndexInfo(command.IndexName);
+                    if (indexInfo != null)
                     {
-                        if (results.HasRows)
+                        if (indexInfo.MaxCharLength > 0)
                         {
-                            ReferenceAttribute[] referenceFields = null;
-
-                            int currentOffset = 0;
-
-                            if (matchValue != null)
+                            var value = (string)matchValue;
+                            if (value.Length > indexInfo.MaxCharLength)
                             {
-                                // convert enums to an int, else the .Equals later check will fail
-                                // this feels a bit kludgey, but for now it's all I can think of
-                                if (matchValue.GetType().IsEnum)
-                                {
-                                    matchValue = (int)matchValue;
-                                }
+                                matchValue = value.Substring(0, indexInfo.MaxCharLength);
+                            }
+                        }
+                    }
+                }
 
-                                if (searchOrdinal < 0)
-                                {
-                                    searchOrdinal = results.GetOrdinal(matchField);
-                                }
+                using (var results = command.ExecuteResultSet(options))
+                {
+                    if (results.HasRows)
+                    {
+                        ReferenceAttribute[] referenceFields = null;
 
-                                if (tableDirect)
+                        int currentOffset = 0;
+
+                        if (matchValue != null)
+                        {
+                            // convert enums to an int, else the .Equals later check will fail
+                            // this feels a bit kludgey, but for now it's all I can think of
+                            if (matchValue.GetType().IsEnum)
+                            {
+                                matchValue = (int)matchValue;
+                            }
+
+                            if (searchOrdinal < 0)
+                            {
+                                searchOrdinal = results.GetOrdinal(matchField);
+                            }
+
+                            if (tableDirect)
+                            {
+                                results.Seek(DbSeekOptions.FirstEqual, new object[] { matchValue });
+                            }
+                        }
+
+                        while (results.Read())
+                        {
+                            if (currentOffset < firstRowOffset)
+                            {
+                                currentOffset++;
+                                continue;
+                            }
+
+                            if (tableDirect && (matchValue != null))
+                            {
+                                // if we have a match value, we'll have seeked to the first match above
+                                // then at this point the first non-match means we have no more matches, so
+                                // we can exit out once we hit the first non-match.
+
+                                // For string we want a case-insensitive search, so it's special-cased here
+                                if (matchValue is string)
                                 {
-                                    results.Seek(DbSeekOptions.FirstEqual, new object[] { matchValue });
+                                    if (string.Compare((string)results[searchOrdinal], (string)matchValue, true) != 0)
+                                    {
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    if (!results[searchOrdinal].Equals(matchValue))
+                                    {
+                                        break;
+                                    }
                                 }
                             }
 
-                            while (results.Read())
+                            object item = Activator.CreateInstance(objectType);
+                            object rowPK = null;
+
+                            // autofill references if desired
+                            if (referenceFields == null)
                             {
-                                if (currentOffset < firstRowOffset)
-                                {
-                                    currentOffset++;
-                                    continue;
-                                }
+                                referenceFields = Entities[entityName].References.ToArray();
+                            }
 
-                                if (tableDirect && (matchValue != null))
-                                {
-                                    // if we have a match value, we'll have seeked to the first match above
-                                    // then at this point the first non-match means we have no more matches, so
-                                    // we can exit out once we hit the first non-match.
 
-                                    // For string we want a case-insensitive search, so it's special-cased here
-                                    if (matchValue is string)
+                            foreach (var field in Entities[entityName].Fields)
+                            {
+                                var value = results[field.Ordinal];
+                                if (value != DBNull.Value)
+                                {
+                                    if (field.DataType == DbType.Object)
                                     {
-                                        if (string.Compare((string)results[searchOrdinal], (string)matchValue, true) != 0)
+                                        if (fillReferences)
                                         {
-                                            break;
+                                            // get serializer
+                                            var itemType = item.GetType();
+                                            var deserializer = GetDeserializer(itemType);
+
+                                            if (deserializer == null)
+                                            {
+                                                throw new MissingMethodException(
+                                                    string.Format("The field '{0}' requires a custom serializer/deserializer method pair in the '{1}' Entity",
+                                                    field.FieldName, entityName));
+                                            }
+
+                                            var @object = deserializer.Invoke(item, new object[] { field.FieldName, value });
+                                            field.PropertyInfo.SetValue(item, @object, null);
                                         }
+                                    }
+                                    else if (field.IsRowVersion)
+                                    {
+                                        // sql stores this an 8-byte array
+                                        field.PropertyInfo.SetValue(item, BitConverter.ToInt64((byte[])value, 0), null);
+                                    }
+                                    else if (field.PropertyInfo.PropertyType.UnderlyingTypeIs<TimeSpan>())
+                                    {
+                                        // SQL Compact doesn't support Time, so we're convert to ticks in both directions
+                                        var valueAsTimeSpan = new TimeSpan((long)value);
+                                        field.PropertyInfo.SetValue(item, valueAsTimeSpan, null);
                                     }
                                     else
                                     {
-                                        if (!results[searchOrdinal].Equals(matchValue))
-                                        {
-                                            break;
-                                        }
+                                        field.PropertyInfo.SetValue(item, value, null);
                                     }
                                 }
+                                //Check if it is reference key to set, not primary.
+                                ReferenceAttribute attr = referenceFields.Where(
+                                    x => x.ReferenceField == field.FieldName).FirstOrDefault();
 
-                                object item = Activator.CreateInstance(objectType);
-                                object rowPK = null;
-
-                                // autofill references if desired
-                                if (referenceFields == null)
+                                if (attr != null)
                                 {
-                                    referenceFields = Entities[entityName].References.ToArray();
+                                    rowPK = value;
                                 }
-
-
-                                foreach (var field in Entities[entityName].Fields)
+                                if (field.IsPrimaryKey)
                                 {
-                                    var value = results[field.Ordinal];
-                                    if (value != DBNull.Value)
-                                    {
-                                        if (field.DataType == DbType.Object)
-                                        {
-                                            if (fillReferences)
-                                            {
-                                                // get serializer
-                                                var itemType = item.GetType();
-                                                var deserializer = GetDeserializer(itemType);
-
-                                                if (deserializer == null)
-                                                {
-                                                    throw new MissingMethodException(
-                                                        string.Format("The field '{0}' requires a custom serializer/deserializer method pair in the '{1}' Entity",
-                                                        field.FieldName, entityName));
-                                                }
-
-                                                var @object = deserializer.Invoke(item, new object[] { field.FieldName, value });
-                                                field.PropertyInfo.SetValue(item, @object, null);
-                                            }
-                                        }
-                                        else if (field.IsRowVersion)
-                                        {
-                                            // sql stores this an 8-byte array
-                                            field.PropertyInfo.SetValue(item, BitConverter.ToInt64((byte[])value, 0), null);
-                                        }
-                                        else if (field.PropertyInfo.PropertyType.UnderlyingTypeIs<TimeSpan>())
-                                        {
-                                            // SQL Compact doesn't support Time, so we're convert to ticks in both directions
-                                            var valueAsTimeSpan = new TimeSpan((long)value);
-                                            field.PropertyInfo.SetValue(item, valueAsTimeSpan, null);
-                                        }
-                                        else
-                                        {
-                                            field.PropertyInfo.SetValue(item, value, null);
-                                        }
-                                    }
-                                    //Check if it is reference key to set, not primary.
-                                    ReferenceAttribute attr = referenceFields.Where(
-                                        x => x.ReferenceField == field.FieldName).FirstOrDefault();
-
-                                    if (attr != null)
-                                    {
-                                        rowPK = value;
-                                    }
-                                    if (field.IsPrimaryKey)
-                                    {
-                                        rowPK = value;
-                                    }
+                                    rowPK = value;
                                 }
+                            }
 
-                                if ((fillReferences) && (referenceFields.Length > 0))
-                                {
-                                    //FillReferences(item, rowPK, referenceFields, true);
-                                    FillReferences(item, rowPK, referenceFields, false);
-                                }
+                            if ((fillReferences) && (referenceFields.Length > 0))
+                            {
+                                //FillReferences(item, rowPK, referenceFields, true);
+                                FillReferences(item, rowPK, referenceFields, false);
+                            }
 
-                                items.Add(item);
+                            items.Add(item);
 
-                                if ((fetchCount > 0) && (items.Count >= fetchCount))
-                                {
-                                    break;
-                                }
+                            if ((fetchCount > 0) && (items.Count >= fetchCount))
+                            {
+                                break;
                             }
                         }
                     }
@@ -459,6 +488,8 @@ namespace OpenNETCF.ORM
             }
             finally
             {
+                if ((!UseCommandCache) && (command != null)) command.Dispose();
+
                 FlushReferenceTableCache();
                 DoneWithConnection(connection, false);
             }
