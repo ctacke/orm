86e81c3f9c4dd6116cc001669d31e2f13821c052
Chris Tacke <ctacke@users.noreply.github.com>
2012-05-11 16:26:12 +0000

    BeforeInsert bug fix Updated version number Moved event args code into separate file

---
+++ OpenNETCF.ORM/EventArgs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace OpenNETCF.ORM
+{
+    public class EntityUpdateArgs : EventArgs
+    {
+        internal EntityUpdateArgs(object item, bool cascadeUpdates, string fieldName) 
+        {
+            Item = item;
+            CascadeUpdates = cascadeUpdates;
+            FieldName = fieldName;
+        }
+
+        public object Item { get; set; }
+        public bool CascadeUpdates { get; set; }
+        public string FieldName { get; set; }
+    }
+
+    public class EntityInsertArgs : EventArgs
+    {
+        internal EntityInsertArgs(object item, bool insertReferences)
+        {
+            Item = item;
+            InsertReferences = insertReferences;
+        }
+
+        public object Item { get; set; }
+        public bool InsertReferences { get; set; }
+    }
+}
