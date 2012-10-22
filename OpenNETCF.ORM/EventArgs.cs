3f2a83b14f14791e2301fecb98fd603a04845999
ctacke_cp <SND\ctacke_cp>
2012-10-22 22:58:30 +0000

    Updates to DataStore events and OnInsert/Delete/Update methods (*interface changes*)

--- OpenNETCF.ORM/EventArgs.cs
+++ OpenNETCF.ORM/EventArgs.cs
@@ -19,13 +19,15 @@ namespace OpenNETCF.ORM
 
     public class EntityUpdateArgs : EventArgs
     {
-        internal EntityUpdateArgs(object item, bool cascadeUpdates, string fieldName) 
+        internal EntityUpdateArgs(string entityName, object item, bool cascadeUpdates, string fieldName) 
         {
+            EntityName = entityName;
             Item = item;
             CascadeUpdates = cascadeUpdates;
             FieldName = fieldName;
         }
 
+        public string EntityName { get; set; }
         public object Item { get; set; }
         public bool CascadeUpdates { get; set; }
         public string FieldName { get; set; }
@@ -33,23 +35,27 @@ namespace OpenNETCF.ORM
 
     public class EntityInsertArgs : EventArgs
     {
-        internal EntityInsertArgs(object item, bool insertReferences)
+        internal EntityInsertArgs(string entityName, object item, bool insertReferences)
         {
+            EntityName = entityName;
             Item = item;
             InsertReferences = insertReferences;
         }
 
+        public string EntityName { get; set; }
         public object Item { get; set; }
         public bool InsertReferences { get; set; }
     }
 
     public class EntityDeleteArgs : EventArgs
     {
-        internal EntityDeleteArgs(object item)
+        internal EntityDeleteArgs(string entityName, object item)
         {
+            EntityName = entityName;
             Item = item;
         }
 
+        public string EntityName { get; set; }
         public object Item { get; set; }
     }
 }
