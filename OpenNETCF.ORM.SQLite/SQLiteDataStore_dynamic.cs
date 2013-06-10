87eccd0e03f59e2306ddaf33fbe39be8ea36fe06
ctacke_cp <SND\ctacke_cp>
2013-06-10 18:51:21 +0000

    Project file updates (Android)

--- OpenNETCF.ORM.SQLite/SQLiteDataStore_dynamic.cs
+++ OpenNETCF.ORM.SQLite/SQLiteDataStore_dynamic.cs
@@ -3,7 +3,24 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+
+#if ANDROID
+// note the case difference between the System.Data.SQLite and Mono's implementation
+using SQLiteCommand = Mono.Data.Sqlite.SqliteCommand;
+using SQLiteConnection = Mono.Data.Sqlite.SqliteConnection;
+using SQLiteParameter = Mono.Data.Sqlite.SqliteParameter;
+using SQLiteDataReader = Mono.Data.Sqlite.SqliteDataReader;
+using SQLiteTransaction = Mono.Data.Sqlite.SqliteTransaction;
+#elif WINDOWS_PHONE
+// ah the joys of an open-source project changing cases on us
+using SQLiteConnection = Community.CsharpSqlite.SQLiteClient.SqliteConnection;
+using SQLiteCommand = Community.CsharpSqlite.SQLiteClient.SqliteCommand;
+using SQLiteParameter = Community.CsharpSqlite.SQLiteClient.SqliteParameter;
+using SQLiteDataReader = Community.CsharpSqlite.SQLiteClient.SqliteDataReader;
+using SQLiteTransaction = Community.CsharpSqlite.SQLiteClient.SqliteTransaction;
+#else
 using System.Data.SQLite;
+#endif
 
 namespace OpenNETCF.ORM
 {
