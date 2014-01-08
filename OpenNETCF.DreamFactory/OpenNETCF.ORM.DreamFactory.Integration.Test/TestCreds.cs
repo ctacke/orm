ed39926a4598595e1f6c8c1e3790e1e358774d7f
ctacke_cp <SND\ctacke_cp>
2014-01-08 01:28:19 +0000

    Another fix

--- OpenNETCF.DreamFactory/OpenNETCF.ORM.DreamFactory.Integration.Test/TestCreds.cs
+++ OpenNETCF.DreamFactory/OpenNETCF.ORM.DreamFactory.Integration.Test/TestCreds.cs
@@ -7,7 +7,8 @@ namespace OpenNETCF.ORM.DreamFactory.Integration.Test
 {
     internal static class TestCreds
     {
-//        public const string UID = "[your username here]";
-//        public const string PWD = "[your password here]";
+
+        public const string UID = "[your username here]";
+        public const string PWD = "[your password here]";
     }
 }
